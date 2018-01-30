3ba4e0944f9a5d0b8ab92a62c973c20b8721ff9e
Joe Sauve <joe@joesauve.com>
2018-01-29 22:59:02 -0600

    Added multiple gist file execution and more validation/messaging

--- step.sh
+++ step.sh
@@ -6,26 +6,74 @@ echo "gist_url: ${gist_url}"
 # Required input validation
 if [[ "${gist_url}" == "" ]]; then
 	echo
-	echo "No script_url provided as environment variable. Terminating..."
+	echo "No gist_url provided as environment variable. Terminating..."
 	echo
 	exit 1
 fi
 
+# Required input validation
+if [[ "${exit_on_failure}" != "0" ]] && [[ "${exit_on_failure}" != "1" ]]; then
+	echo
+	echo "exit_on_failure must be set to either 0 or 1. Terminating..."
+	echo
+	exit 1
+fi
 
-echo
-echo "---------------------------------------------------"
-echo "--- Executing script: ${script_url}"
-echo
-
-curl -sSL "${gist_url}" | xml select -t -v /html/body | xmllint --xpath "string(//a[text()='Raw']/@href)" - | bash
-res_code=$?
-if [ ${res_code} -ne 0 ] ; then
-	echo "--- [!] The script returned with an error code: ${res_code}"
-	echo "---------------------------------------------------"
+# validate Gist URL
+if [[ "${gist_url}" != "http://gist.github.com"* ]] && [[ "${gist_url}" != "https://gist.github.com"* ]];then
+	echo
+	echo "gist_url must be a valid Gist URL (containing 'gist.github.com').Terminating... "
+	echo
 	exit 1
 fi
 
 echo
-echo "--- Script returned with a success code - OK"
 echo "---------------------------------------------------"
+echo "--- Fetching raw Gist URL(s) from: ${gist_url}"
+echo
+echo -- "Warning messages from tidy command (disregard):"
+
+temp_doc=$(curl -sSL "${gist_url}" | grep -Eoi '<a [^>]+>Raw</a>' | tidy -quiet)
+
+file_count=$(echo $temp_doc | xmllint --html --xpath "count(//a/@href)" -)
+
+echo
+echo "--- # of Gist files found: $file_count"
+echo "---------------------------------------------------"
+
+declare -a raw_urls
+
+for (( i=1; i <= $file_count; i++ )); do 
+    raw_urls[$i]="$(echo $temp_doc | xmllint --html --xpath 'string(//a['$i']/@href)' -)"
+done
+
+j=1
+for i in "${raw_urls[@]}"
+do
+
+	raw_url="https://gist.githubusercontent.com$i"
+	echo
+	echo "---------------------------------------------------"
+	echo "--- Executing remote script #$j: $i"
+	echo
+	curl -sSL "${raw_url}" | bash
+	res_code=$?
+	if [ ${res_code} -ne 0 ] ; then
+		echo
+		echo "--- [!] Script #$j returned with an error code: ${res_code}"
+		echo "---------------------------------------------------"
+		if [[ "${exit_on_failure}" == "1" ]]; then
+			exit 1
+		fi
+		j=$(($j+1))
+	else
+		echo
+		echo "--- Script #$j returned with a success code - OK"
+		echo "---------------------------------------------------"
+		echo
+		j=$(($j+1))
+	fi
+
+done
+
 exit 0
